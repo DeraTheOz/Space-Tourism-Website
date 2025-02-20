e597344403e72dd2d6a4409f388d42c9ca93d649
MannexDera <emmanuelchi545@gmail.com>
2025-02-20 17:48:34 +0100

    refactor transformation methods

--- src/js/models/data-model.js
+++ src/js/models/data-model.js
@@ -11,18 +11,46 @@ const dataModel = function () {
 
     const getCrew = () => {
         return data.crew.reduce((acc, member) => {
-            const key = member.name.toLowerCase().replace(/\s+/g, '-');
-            console.log(key);
-            acc[key] = member;
+            const key = member.name.trim().split(/\s+/g);
+            if (!key.length) return '';
+
+            const firstWord = key[0].toLowerCase();
+            const secondWord = key
+                .slice(1)
+                .map(
+                    (word) =>
+                        word.charAt(0).toUpperCase() +
+                        word.slice(1).toLowerCase()
+                );
+
+            const newKey = firstWord + secondWord.join('');
+
+            acc[newKey] = member;
             return acc;
         }, {});
     };
 
     const getTechnology = () => {
         return data.technology.reduce((acc, tech) => {
-            const key = tech.name.toLowerCase().replace(/\s+/g, '-');
-            console.log(key);
-            acc[key] = tech;
+            const key = tech.name.trim().split(/\s+/g);
+            let newKey;
+
+            if (key.length <= 1) {
+                newKey = key[0].toLowerCase();
+            } else {
+                const firstWord = key[0].toLowerCase();
+                const secondWord = key
+                    .slice(1)
+                    .map(
+                        (word) =>
+                            word.charAt(0).toUpperCase() +
+                            word.slice(1).toLowerCase()
+                    );
+
+                newKey = firstWord + secondWord.join('');
+            }
+
+            acc[newKey] = tech;
             return acc;
         }, {});
     };
