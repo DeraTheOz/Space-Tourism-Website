05c1d58399c50fe878c07deaa63ed7796c263eac
MannexDera <emmanuelchi545@gmail.com>
2025-02-21 22:08:55 +0100

    add crew view functionality

--- src/js/views/crew-view.js
+++ src/js/views/crew-view.js
@@ -1,25 +1,109 @@
 import dataModel from '../models/data-model.js';
-import imageMoonPng from '../../assets/destination/image-moon.png';
-import imageMoonWebp from '../../assets/destination/image-moon.webp';
-import imageMarsPng from '../../assets/destination/image-mars.png';
-import imageMarsWebp from '../../assets/destination/image-mars.webp';
-import imageEuropaPng from '../../assets/destination/image-europa.png';
-import imageEuropaWebp from '../../assets/destination/image-europa.webp';
-import imageTitanPng from '../../assets/destination/image-titan.png';
-import imageTitanWebp from '../../assets/destination/image-titan.webp';
+import imageDouglasPng from '../../assets/crew/image-douglas-hurley.png';
+import imageDouglasWebp from '../../assets/crew/image-douglas-hurley.webp';
+import imageMarkPng from '../../assets/crew/image-mark-shuttleworth.png';
+import imageMarkWebp from '../../assets/crew/image-mark-shuttleworth.webp';
+import imageVictorPng from '../../assets/crew/image-victor-glover.png';
+import imageVictorWebp from '../../assets/crew/image-victor-glover.webp';
+import imageAnoushehPng from '../../assets/crew/image-anousheh-ansari.png';
+import imageAnoushehWebp from '../../assets/crew/image-anousheh-ansari.webp';
+
+import revertCamelCase from '../utils/revertCamelCase.js';
 
 const images = {
-    moon: { png: imageMoonPng, webp: imageMoonWebp },
-    mars: { png: imageMarsPng, webp: imageMarsWebp },
-    europa: { png: imageEuropaPng, webp: imageEuropaWebp },
-    titan: { png: imageTitanPng, webp: imageTitanWebp }
+    douglasHurley: { png: imageDouglasPng, webp: imageDouglasWebp },
+    markShuttleworth: { png: imageMarkPng, webp: imageMarkWebp },
+    victorGlover: { png: imageVictorPng, webp: imageVictorWebp },
+    anoushehAnsari: { png: imageAnoushehPng, webp: imageAnoushehWebp }
 };
 
 const crew = dataModel.getCrew();
-console.log(crew);
+console.log(crew['douglasHurley']);
 
 const crewView = function () {
-    const render = (crewName = 'davidHurley') => {};
+    const render = (crewName = 'douglasHurley') => {
+        console.log('Crew Name', crewName);
+
+        const main = document.querySelector('main');
+        const crewContainer = document.querySelector('.crew__container');
+
+        const crewMember = crew[crewName] || crew['douglasHurley'];
+
+        if (!crewContainer) {
+            document.body.className = 'crew';
+
+            main.innerHTML = `
+            <section class="crew__hero">
+                <p class="crew__intro"><span class="crew__step">01</span> Pick your crew</p>
+                
+                <div class="crew__container">
+                    ${crewContent(crew, crewName)}
+                </div>
+            </section>`;
+            return;
+        }
+
+        // Fade out, update content and fade back in
+        crewContainer.classList.add('fade-out');
+        setTimeout(() => {
+            crewContainer.innerHTML = crewContent(crew, crewName);
+            crewContainer.classList.remove('fade-out');
+        }, 500);
+    };
+
+    const crewContent = function (crew, crewName) {
+        const memberName = revertCamelCase(crewName);
+
+        return ` 
+            <div class="crew__details">
+                <div class="crew__details--box">
+                    <p class="crew__role">${crew[crewName].role}</p>
+                    <h1 class="crew__name">${memberName}</h1>
+                    <p class="crew__summary">
+                        ${crew[crewName].bio}
+                    </p>
+                </div>
+
+                <ul class="crew__slides">${Object.values(crew)
+                    .map(
+                        (name) => `
+                        <li class="slide ${
+                            name === crew[crewName] ? 'slide--active' : ''
+                        }" data-name=${crewName}>
+                        </li>
+                    `
+                    )
+                    .join('')}
+                </ul>
+            </div>
+            <div class="crew__image--box">
+                <picture>
+                    <source srcset="${
+                        images[crewName].webp
+                    }" type="image/webp" />
+                    <img class="crew__image"
+                        src="${images[crewName].png}"
+                        alt="Crew Commander Douglas Hurley"
+                    />
+                </picture>
+            </div>
+        `;
+    };
+
+    (function handleCrewSwitch() {
+        document.querySelector('main').addEventListener('click', (e) => {
+            if (!e.target.classList.contains('slide')) return;
+
+            const clickedSlide = e.target.dataset.name;
+            if (!clickedSlide) return;
+
+            console.log(clickedSlide);
+
+            render(clickedSlide);
+        });
+    })();
 
     return { render };
 };
+
+export default crewView();
