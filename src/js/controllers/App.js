0805929a4e2214d538a1cd1cbdc4ef4ca35e84b3
MannexDera <emmanuelchi545@gmail.com>
2025-02-24 23:00:17 +0100

    update App

--- src/js/controllers/App.js
+++ src/js/controllers/App.js
@@ -1,36 +1,8 @@
-import * as Header from '../utils/header-actions.js';
 import Router from '../routes/Router.js';
-import homeView from '../views/home-view.js';
-import destinationView from '../views/destination-view.js';
-import crewView from '../views/crew-view.js';
-import technologyView from '../views/technology-view.js';
-
-// const init = () => {
-//     Header.handleMenuButtonClicks();
-//     Header.handleNavLinksClick();
-
-//     Router.addRoute('/', homeView.render);
-//     Router.addRoute('/destination', destinationView.render);
-//     Router.addRoute('/crew', crewView.render);
-//     Router.addRoute('/technology', technologyView.render);
-//     Router.init();
-
-//     console.log('App Initialized 🚀');
-// };
-// init();
 
 const init = () => {
-    Header.handleMenuButtonClicks();
-    Header.handleNavLinksClick();
-
-    console.log('WINDOW', window);
-    window.app = {};
-    console.log('WINDOW.APP', window.app);
-    app.router = Router;
-    console.log('APP.ROUTER', app.router);
-
     window.addEventListener('DOMContentLoaded', () => {
-        app.router.init();
+        Router.init();
     });
 };
 init();
