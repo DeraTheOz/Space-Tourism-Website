8599582417b4e420b6386f3d254f4e0260d1c544
MannexDera <emmanuelchi545@gmail.com>
2025-02-26 22:49:54 +0100

    update app initialization

--- src/js/controllers/App.js
+++ src/js/controllers/App.js
@@ -1,6 +1,12 @@
 import Router from '../routes/Router.js';
 
 const init = () => {
+    // Route to destination
+    const exploreBtn = document.querySelector('.hero__btn');
+    exploreBtn.addEventListener('click', () => {
+        Router.navigateTo('/destination');
+    });
+
     window.addEventListener('DOMContentLoaded', () => {
         Router.init();
     });
