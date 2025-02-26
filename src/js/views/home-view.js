88c166bb5f552530d564eec25283bf56dea0b6e1
MannexDera <emmanuelchi545@gmail.com>
2025-02-26 22:51:06 +0100

    update home view

--- src/js/views/home-view.js
+++ src/js/views/home-view.js
@@ -1,10 +1,53 @@
 import Router from '../routes/Router.js';
 
+// const homeView = function () {
+//     const render = () => {
+//         document.body.className = 'home';
+//         const main = document.querySelector('main');
+//         if (!main.innerHTML.trim()) {
+//             main.innerHTML = `
+//             <section class="hero">
+//                 <p class="hero__intro">So, you want to travel to</p>
+//                 <div class="hero__container">
+//                     <div class="hero__box">
+//                         <h1 class="hero__title">Space</h1>
+//                         <p class="hero__summary">
+//                             Let’s face it; if you want to go to space, you
+//                             might as well genuinely go to outer space and
+//                             not hover kind of on the edge of it. Well sit
+//                             back, and relax because we’ll give you a truly
+//                             out of this world experience!
+//                         </p>
+//                     </div>
+
+//                     <div class="hero__button--box">
+//                         <button type="button" class="cta-btn hero__btn">
+//                             Explore
+//                         </button>
+//                     </div>
+//                 </div>
+//             </section>
+//             `;
+//         }
+
+//         // Route to destination
+//         // const exploreBtn = document.querySelector('.hero__btn');
+//         // exploreBtn.addEventListener('click', () => {
+//         //     Router.navigateTo('/destination');
+//         // });
+//     };
+
+//     return { render };
+// };
+// export default homeView();
+
 const homeView = function () {
     const render = () => {
         document.body.className = 'home';
+        const main = document.querySelector('main');
+        main.innerHTML = '';
 
-        document.querySelector('main').innerHTML = `
+        main.innerHTML = `
         <section class="hero">
             <p class="hero__intro">So, you want to travel to</p>
             <div class="hero__container">
@@ -16,7 +59,7 @@ const homeView = function () {
                         a truly out of this world experience!
                     </p>
                 </div>
-    
+
                 <div class="hero__button--box">
                     <button type="button" class="cta-btn hero__btn">Explore</button>
                 </div>
@@ -33,5 +76,4 @@ const homeView = function () {
 
     return { render };
 };
-
 export default homeView();
