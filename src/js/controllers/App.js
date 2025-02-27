51ccf44fdad6d957d2660afcf5f3251f0dfb4ac4
MannexDera <emmanuelchi545@gmail.com>
2025-02-27 14:35:31 +0100

    fix app init

--- src/js/controllers/App.js
+++ src/js/controllers/App.js
@@ -1,13 +1,6 @@
 import Router from '../routes/Router.js';
 
 const init = () => {
-    // Route to destination
-    const exploreBtn = document.querySelector('.hero__btn');
-    exploreBtn.addEventListener('click', () => {
-        Router.navigateTo('/destination');
-    });
-
     Router.init();
-    // window.addEventListener('DOMContentLoaded', () => {});
 };
 init();
